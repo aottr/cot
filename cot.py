4a529da2fbe80a988304a0b3e8172f6af85a06e6
Dustin Kröger <me@staubrein.com>
2021-01-15 13:09:44 +0100

    fixed issues, working now

--- cot.py
+++ cot.py
@@ -34,7 +34,8 @@ roomSetups = {
 }
 
 
-lcd.font(lcd.FONT_DejaVu40)
+lcd.font(lcd.FONT_DejaVu24)
+lcd.setBrightness(10)
 
 #humidity reference value
 GOOD_HUMIDITY = 40.0
@@ -48,9 +49,9 @@ ventilationNeeded = False
 def readHumidity():
 	# data is 4 bites
 	buf = bytearray(5)
-	i2c.readfrom_into(0x5c, buf, stop=True, /)
-	if (buf[0] + buf[1] + buf[2] + buf[3]) & 0xff != buf[4]:
-		raise Exception("checksum error")
+	i2c.readfrom_into(0x5c, buf)
+	#if (buf[0] + buf[1] + buf[2] + buf[3]) & 0xff != buf[4]:
+	#	raise Exception("checksum error")
 
 	# first two byte are humidity, next two byte are temperature
 	return (buf[0] + buf[1]) * 0.1
@@ -60,68 +61,72 @@ def showDisplay(humidity):
 
 	# set colourcodes
 	colourBad = 0xff0000
-	colourGood = 0x00ff00
+	colourGood = 0x006400
 
-	if (humidity >= goodHumidity):
-		lcd.rect(0, 0, 320, 60, color=colourGood)
+	if (humidity >= GOOD_HUMIDITY):
+		lcd.rect(0, 0, 320, 60, colourGood, colourGood)
 	else:
-		lcd.rect(0, 0, 320, 60, color=colourBad)
-	lcd.print('humidity: {}%'.format(humidity), 0, 0, 0xffffff)
+		lcd.rect(0, 0, 320, 60, colourBad, colourBad)
+
+	lcd.print('humidity: {}%'.format(humidity), 0, 13, 0xffffff)
 
 	if ventilationNeeded:
-		lcd.rect(0, 60, 320, 60, color=colourBad)
-		lcd.print('ventilation needed', 0, 60, 0xffffff)
+		lcd.rect(0, 60, 320, 60, colourBad, colourBad)
+		lcd.print('ventilation needed', 0, 73, 0xffffff)
 	else:
-		lcd.rect(0, 60, 320, 60, color=colourGood)
-		lcd.print('no ventilation needed', 0, 60, 0xffffff)
+		lcd.rect(0, 60, 320, 60, colourGood, colourGood)
+		lcd.print('no ventilation needed', 0, 73, 0xffffff)
 
 	if stateOut1:
-		lcd.rect(0, 120, 320, 60, color=colourGood)
-		lcd.print('output1 state: on', 0, 120, 0xffffff)
+		lcd.rect(0, 120, 320, 60, colourGood, colourGood)
+		lcd.print('output1 state: on', 0, 133, 0xffffff)
 	else:
-		lcd.rect(0, 120, 320, 60, color=colourBad)
-		lcd.print('output1 state: off', 0, 120, 0xffffff)
+		lcd.rect(0, 120, 320, 60, colourBad, colourBad)
+		lcd.print('output1 state: off', 0, 133, 0xffffff)
 
 	if stateOut2:
-		lcd.rect(0, 180, 320, 60, color=colourGood)
-		lcd.print('output2 state: on', 0, 180, 0xffffff)
+		lcd.rect(0, 180, 320, 60, colourGood, colourGood)
+		lcd.print('output2 state: on', 0, 193, 0xffffff)
 	else:
-		lcd.rect(0, 180, 320, 60, color=colourBad)
-		lcd.print('output2 state: off', 0, 180, 0xffffff)
+		lcd.rect(0, 180, 320, 60, colourBad, colourBad)
+		lcd.print('output2 state: off', 0, 193, 0xffffff)
 
 def sendMQTT(humidity):
 	topic = str('lab/03/{}'.format(ROOM_ID))
-	message = str('\{"humidity":"{}","ventilationNeeded":"{}","stateOut1":"{}","stateOut2":"{}"\}'.format(str(humidity), str(ventilationNeeded), str(stateOut1), str(stateOut2)))
+	message = str('{{"humidity":"{}","ventilationNeeded":"{}","stateOut1":"{}","stateOut2":"{}"}}'.format(str(humidity), str(ventilationNeeded), str(stateOut1), str(stateOut2)))
 	mqtt.publish(topic, message)
 
 def onOut1():
 	p12.on()
+	global stateOut1
 	stateOut1 = True
 
 def offOut1():
 	p12.off()
+	global stateOut1
 	stateOut1 = False
 
 def onOut2():
 	p13.on()
+	global stateOut2
 	stateOut2 = True
 
 def offOut2():
 	p13.off()
+	global stateOut2
 	stateOut2 = False
 
 while True:
 	humidity = readHumidity()
-
 	# if bad humidity
-	if (humidity < goodHumidity):
-		if (ventilationNeeded == False && stateOut1 == False && stateOut2 == False):
+	if (humidity < GOOD_HUMIDITY):
+		if (ventilationNeeded == False and stateOut1 == False and stateOut2 == False):
 			ventilationNeeded = True
 			onOut1()
 			onOut2()
 	# very good -> turn off vents
 	elif (humidity > 60.0):
-		if (ventilationNeeded == True && stateOut1 == True && stateOut2 == True):
+		if (ventilationNeeded == True and stateOut1 == True and stateOut2 == True):
 			ventilationNeeded = False
 			offOut1()
 			offOut2()
